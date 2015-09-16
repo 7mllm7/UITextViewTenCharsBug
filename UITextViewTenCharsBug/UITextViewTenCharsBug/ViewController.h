3c7b871ceb8f1ef5776bef310739d7060343491e
ML <moti.l@mediapplications.com>
2015-09-16 16:54:42 +0300

    New single view app Xcode project

---
+++ UITextViewTenCharsBug/UITextViewTenCharsBug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITextViewTenCharsBug
+//
+//  Created by MLmediapps on 9/16/15.
+//  Copyright © 2015 ML. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
